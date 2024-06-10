22f9032b0d72e53633470e9e3274704b4c590e40
Stha@2003 <ss52021321@student.ku.edu.n>
2024-06-10 16:59:45 +0545

    memory management banking problem

--- customer.py
+++
@@ -1,9 +0,0 @@
-class Customer:
-    def __init__(self, id, arrival_time):
-        self.id = id
-        self.arrival_time = arrival_time
-        self.start_time = None
-        self.end_time = None
-
-    def __repr__(self):
-        return f"Customer(id={self.id}, arrival_time={self.arrival_time}, start_time={self.start_time}, end_time={self.end_time})"
